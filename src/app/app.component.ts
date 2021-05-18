89d364fc8c802f00abef91a798767b24aa42c4ec
Joel Yesupriya <jyesupriya@catalyte.io>
2021-05-18 14:47:40 -0400

    completed work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { UsersService } from "./users.service";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'journey';
+  title = "journey";
+  result: String;
+  submissionForm = new FormGroup({
+    fName: new FormControl("", Validators.required),
+    lName: new FormControl("", Validators.required),
+    email: new FormControl("", Validators.required),
+  });
+
+  constructor(private user: UsersService) {
+    this.getData();
+  }
+
+  get fName() {
+    return this.submissionForm.get("fName");
+  }
+  get lName() {
+    return this.submissionForm.get("lName");
+  }
+  get email() {
+    return this.submissionForm.get("email");
+  }
+
+  getData() {
+    this.user.getData().subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  postData(user) {
+    // this.user.postData(user).subscribe(data => this.user.push(data));
+    this.user.postData(user);
+    this.result = "Thank you for submitting," + " " + user.fName + "!";
+  }
 }
---
+++ src/app/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root",
+})
+export class UsersService {
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    let url = "https://jsonplaceholder.typicode.com/users/";
+    return this.http.get(url);
+  }
+
+  postData(user) {
+    let url = "https://jsonplaceholder.typicode.com/users/";
+    // return this.http.post(url, user);
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    console.log(user);
+  }
+}
